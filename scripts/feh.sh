d8797b58d205f20212ea35139d9622398404c79f
Caleb Hoover <choover11@gmail.com>
2018-02-13 08:26:18 -0800

    x file tweaks

--- scripts/feh.sh
+++ scripts/feh.sh
@@ -1,4 +1,4 @@
 #feh --bg-scale ~/Pictures/Wallpapers/Boba\ Fett.jpg --bg-scale ~/Pictures/Wallpapers/Darth\ Vader.jpg
 #feh --bg-scale ~/Pictures/Wallpapers/endor.jpg --bg-scale ~/Pictures/Wallpapers/endor.jpg
 
-feh --bg-scale ~/Pictures/wallpapers/Rubik\'s_Cube_Patent.jpg
+feh --bg-scale ~/Pictures/wallpapers/pexels-photo-256112.jpeg
