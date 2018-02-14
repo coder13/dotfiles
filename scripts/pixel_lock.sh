5e83f501228338c6e33105f0d197838cb7c6c587
Caleb Hoover <choover11@gmail.com>
2018-02-14 11:16:29 -0800

    stuff

--- scripts/pixel_lock.sh
+++ scripts/pixel_lock.sh
@@ -3,4 +3,4 @@ FILE=/tmp/screen.png
 scrot $FILE
 mogrify -scale 10% -scale 1000% $FILE
 i3lock -i $FILE
-#sleep 60; pgrep i3lock && xset dpms force off
+sleep 60; pgrep i3lock && xset dpms force off
